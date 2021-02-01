06be9b4e3c0400b137efdce5b7ae2fd759cb16db
SJKim <worldsjk1990@gmail.com>
2021-02-01 17:18:00 +0900

    handle image uploading

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,7 +6,7 @@ import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 
-const Maker = ({ authService }) => {
+const Maker = ({ FileInput, authService }) => {
   const [cards, setCards] = useState({
     1: {
       id: "1",
@@ -80,6 +80,7 @@ const Maker = ({ authService }) => {
       <Header onLogout={onLogout} />
       <div className={styles.container}>
         <Editor
+          FileInput={FileInput}
           cards={cards}
           addCard={createOrUpdateCard}
           updateCard={createOrUpdateCard}
